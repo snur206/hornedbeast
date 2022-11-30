cf961ee47d0ae54e773d25c5bd296208adf34cac
SHARMARKE NUR <Snur206@yahoo.com>
2022-11-29 17:23:31 -0800

    lab 2 done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,25 @@
 import React from "react";
 import HornedBeast from "./HornedBeasts";
+import hbData from "../data.json";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 class Main extends React.Component {
     render() {
         return (
-            <>
-                <HornedBeast
-                    title="ram"
-                    description="three rams walking"
-                    imgurl="https://images.unsplash.com/photo-1507668534048-612633783b70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80"
-                />
-
-                <HornedBeast
-                    title="moose"
-                    description="moose sitting"
-                    imgurl="https://images.unsplash.com/photo-1582002834723-2256d33da100?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                />
-            </>
-
+<Container>
+    <Row xs={2} sm={3} md={4} lg={5} >
+        {hbData.map(hbInfo => (
+            <Col>
+            <HornedBeast key={hbInfo._id} id={hbInfo._id} title={hbInfo.title} imgUrl={hbInfo.image_url} description={hbInfo.description} keyword={hbInfo.keyword} horns={hbInfo.horns}/>
+                    </Col>))} 
+                    </Row> 
+                </Container>
+                
         )
     }
 }         
-
 export default Main;
